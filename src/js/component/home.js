25b9ea755fed5ef8b0c0a93436fa753e6a192a9b
avalentinagd <avalentinagd@gmail.com>
2020-08-16 13:43:28 +0000

    Incluyendo tareas a la lista

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,81 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { Fragment, useState } from "react";
 
 //create your first component
 export function Home() {
+	const [taskList, setTaskList] = useState([]);
+
+	const addTask = e => {
+		if (e.key === "Enter") {
+			console.log(e.target.value);
+			setTaskList([...taskList, e.target.value]);
+		}
+	};
+	console.log(taskList);
+
+	const createUser = () => {
+		fetch("http://assets.breatheco.de/apis/fake/todos/user/avalentinagd"),
+			{
+				method: "POST",
+				mode: "cors",
+				redirect: "follow",
+				headers: new Headers({
+					"Content-Type": "text/plain"
+				}).then(function() {
+					/* manejar la respuesta */
+				})
+			};
+	};
+
+	const ChangeUser = () => {
+		fetch("http://assets.breatheco.de/apis/fake/todos/user/avalentinagd"),
+			{
+				method: "PUT",
+				body: JSON.stringify(data),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+				.then(res => res.json())
+				.then(response =>
+					console.log("Success:", JSON.stringify(response))
+				)
+				.catch(error => console.error("Error:", error));
+	};
+
+	//function endTask(index){
+	//const newtaskList = taskList.filter((index) => index !== id);
+
+	//setTaskList(newtaskList);
+
+	//{taskList.map((theTask, index) => {
+	//							return <li key={index}>{theTask}</li>;
+	//						})}
+	//<button type="button" onClick={() => endTask(index)}>x</button>
+	//}
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<Fragment>
+			<div className="row title container-fluid">
+				<h1>Todos</h1>
+			</div>
+			<div className="container ">
+				<div className="row taskList container mx-auto">
+					<input
+						className="enterTask"
+						placeholder="Enter your task"
+						onKeyPress={e => addTask(e)}
+					/>
+
+					<div className="row task container mx-auto border">
+						<ol className="col-12">
+							{taskList.map((theTask, index) => {
+								return <li key={index}>{theTask}</li>;
+							})}
+						</ol>
+					</div>
+					<div className="row footer">N Items left</div>
+				</div>
+			</div>
+		</Fragment>
 	);
 }
